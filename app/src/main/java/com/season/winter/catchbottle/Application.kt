d2dc863ee41359f15eab6fb45ed0133013bb52ba
hun73877 <hun230402@gmail.com>
2023-07-10 08:14:13 +0900

    add module core_design_resource

--- app/src/main/java/com/season/winter/catchbottle/Application.kt
+++ app/src/main/java/com/season/winter/catchbottle/Application.kt
@@ -2,12 +2,14 @@ package com.season.winter.catchbottle
 
 import android.app.Application
 import com.season.winter.firestore.FireStoreConnectTest
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class Application: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        FireStoreConnectTest().test()
+//        FireStoreConnectTest().test()
     }
 
 }
