a65342e471f750877ca642452a71572f4cade904
hun73877 <hun230402@gmail.com>
2023-07-29 22:13:04 +0900

    fix some code

--- repository/screen/src/main/java/com/season/winter/screen/fragment/navigationMain/home/di/HomeNavigationRepositoryImpl.kt
+++ repository/screen/src/main/java/com/season/winter/screen/fragment/navigationMain/home/di/HomeNavigationRepositoryImpl.kt
@@ -2,23 +2,7 @@ package com.season.winter.screen.fragment.navigationMain.home.di
 
 import com.season.winter.screen.fragment.navigationMain.home.local.HomeNavigationDummyDao
 import com.season.winter.ui.model.fragment.home.HomeItem
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
-import javax.inject.Singleton
-
-@Module
-@InstallIn(SingletonComponent::class)
-object HomeNavigationRepositoryModule {
-
-    @Provides
-    @Singleton
-    fun provideDummyRepository(dao: HomeNavigationDummyDao): HomeNavigationRepositoryImpl {
-        return HomeNavigationRepositoryImpl(dao)
-    }
-}
 
 class HomeNavigationRepositoryImpl @Inject constructor(
     private val dummyDao: HomeNavigationDummyDao
