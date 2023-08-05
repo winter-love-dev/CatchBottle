caef3fe16982f1bf923c32ecf293734204f186db
hun73877 <hun230402@gmail.com>
2023-08-06 04:14:30 +0900

    remove method CatchBottleCipherHelperService.init(context: Context)

--- core/common/src/main/java/com/season/winter/common/util/sharedPrefrences/securePreferences/Rsa2048CipherHelper.kt
+++ core/common/src/main/java/com/season/winter/common/util/sharedPrefrences/securePreferences/Rsa2048CipherHelper.kt
@@ -7,30 +7,30 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class Rsa2048CipherHelper @Inject constructor(
+    @ApplicationContext context: Context,
     private val rsa2048: Rsa2048Algorithm
 ): CatchBottleCipherHelperService {
 
     private var isSupported = false
 
-    override fun init(
-        @ApplicationContext context: Context
-    ) {
-
-        if (isSupported) return
-
-        val alias = "${context.packageName}.rsakeypairs"
-        isSupported = rsa2048.create(alias)
+    init {
+        if (isSupported.not()) {
+            val alias = "${context.packageName}.rsakeypairs"
+            isSupported = rsa2048.create(alias)
+        }
     }
 
     override fun encrypt(plainText: String): String {
-        if (!isSupported)
-            return plainText
-        return rsa2048.encrypt(plainText)
+        return if (isSupported.not())
+            plainText
+        else
+            rsa2048.encrypt(plainText)
     }
 
     override fun decrypt(base64EncryptedCipherText: String): String {
-        if (!isSupported)
-            return base64EncryptedCipherText
-        return rsa2048.decrypt(base64EncryptedCipherText)
+        return if (isSupported.not())
+            base64EncryptedCipherText
+        else
+            rsa2048.decrypt(base64EncryptedCipherText)
     }
 }
