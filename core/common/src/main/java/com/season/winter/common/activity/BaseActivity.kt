7d20c8581f5df7a6511bc5fc35d40d67e3cdae5d
hun73877 <hun230402@gmail.com>
2023-10-10 13:29:07 +0900

    delete meaningless code

--- core/common/src/main/java/com/season/winter/common/activity/BaseActivity.kt
+++ core/common/src/main/java/com/season/winter/common/activity/BaseActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.lifecycleScope
 
 abstract class BaseActivity<T : ViewDataBinding>(
     private val layoutResourceId: Int
